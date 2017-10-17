13e7e4045456cee43666ab49c6f953df90de0f5b
mattyanselmo <mkullowatz@gmail.com>
2017-10-17 08:02:37 -0700

    Moved connectonline.R to IgnoreList

--- ConnectOnline.R
+++
@@ -1,7 +0,0 @@
-library(rsconnect)
-rsconnect::setAccountInfo(name='asashinyapps',
-                          token='46FBBDA6673FC6E11B42172A4E39534C',
-                          secret='5pG2TDGgzjNdcXKVR1PHXdbuorjIvLgmd3Ub65G+')
-rsconnect::deployApp(getwd(), account = 'asashinyapps')
-
-
