d509915df1a1c07efd42b9621e3690f716e67ea6
MKullowatz <mkullowatz@gmail.com>
2018-02-13 21:42:31 -0800

    group_by thirds

--- 02_CompressPlayerPassing.R
+++ 02_CompressPlayerPassing.R
@@ -5,26 +5,13 @@ passing <- readRDS("IgnoreList/AllPassingData.rds")
 
 ## balance predictions to actual by zone
 passing <- passing %>%
-  mutate(section = ifelse(x < 115/3, "Def",
+  mutate(third = ifelse(x < 115/3, "Def",
                           ifelse(x < 115*2/3, "Mid", "Att")))
 
 player.stats <- passing %>%
-  group_by(passer, year, team) %>%
+  group_by(passer, year, team, third) %>%
   summarize(N = n(),
-            `Comp%` = sum(success)/N,
-            `Exp%` = mean(success.pred),
-            Score = `Comp%` - `Exp%`,
-            `N(Def)` = sum(section == "Def"),
-            `%(Def)` = sum(success[section == "Def"])/`N(Def)`,
-            `Exp%(Def)` = mean(success.pred[section == "Def"]),
-            `Score(Def)` = `%(Def)` - `Exp%(Def)`,
-            `N(Mid)` = sum(section == "Mid"),
-            `%(Mid)` = sum(success[section == "Mid"])/`N(Mid)`,
-            `Exp%(Mid)` = mean(success.pred[section == "Mid"]),
-            `Score(Mid)` = `%(Mid)` - `Exp%(Mid)`,
-            `N(Att)` = sum(section == "Att"),
-            `%(Att)` = sum(success[section == "Att"])/`N(Att)`,
-            `Exp%(Att)` = mean(success.pred[section == "Att"]),
-            `Score(Att)` = `%(Att)` - `Exp%(Att)`)
+            success = sum(success),
+            exp = sum(success.pred))
 
 saveRDS(player.stats, "IgnoreList/xPassingByPlayer.rds")
--- 02_CompressTeamPassing.R
+++ 02_CompressTeamPassing.R
@@ -1,50 +1,24 @@
-# Condense player passing data for web app
+# Condense team passing data for web app
 
 # Read dataset ####
 passing <- readRDS("IgnoreList/AllPassingData.rds")
 
 ## balance predictions to actual by zone
 passing <- passing %>%
-  mutate(section = ifelse(x < 115/3, "Def",
-                          ifelse(x < 115*2/3, "Mid", "Att")))
+  mutate(third = ifelse(x < 115/3, "Def",
+                        ifelse(x < 115*2/3, "Mid", "Att")))
 
 team.stats.offense <- passing %>%
-  group_by(team, year) %>%
+  group_by(year, team, third) %>%
   summarize(N = n(),
-            `Comp%` = sum(success)/N,
-            `Exp%` = mean(success.pred),
-            Score = `Comp%` - `Exp%`,
-            `N(Def)` = sum(section == "Def"),
-            `%(Def)` = sum(success[section == "Def"])/`N(Def)`,
-            `Exp%(Def)` = mean(success.pred[section == "Def"]),
-            `Score(Def)` = `%(Def)` - `Exp%(Def)`,
-            `N(Mid)` = sum(section == "Mid"),
-            `%(Mid)` = sum(success[section == "Mid"])/`N(Mid)`,
-            `Exp%(Mid)` = mean(success.pred[section == "Mid"]),
-            `Score(Mid)` = `%(Mid)` - `Exp%(Mid)`,
-            `N(Att)` = sum(section == "Att"),
-            `%(Att)` = sum(success[section == "Att"])/`N(Att)`,
-            `Exp%(Att)` = mean(success.pred[section == "Att"]),
-            `Score(Att)` = `%(Att)` - `Exp%(Att)`)
+            success = sum(success),
+            exp = sum(success.pred))
 
 team.stats.defense <- passing %>%
-  group_by(team.1, year) %>%
+  group_by(year, team.1, third) %>%
   summarize(N = n(),
-            `Comp%` = sum(success)/N,
-            `Exp%` = mean(success.pred),
-            Score = `Comp%` - `Exp%`,
-            `N(Def)` = sum(section == "Def"),
-            `%(Def)` = sum(success[section == "Def"])/`N(Def)`,
-            `Exp%(Def)` = mean(success.pred[section == "Def"]),
-            `Score(Def)` = `%(Def)` - `Exp%(Def)`,
-            `N(Mid)` = sum(section == "Mid"),
-            `%(Mid)` = sum(success[section == "Mid"])/`N(Mid)`,
-            `Exp%(Mid)` = mean(success.pred[section == "Mid"]),
-            `Score(Mid)` = `%(Mid)` - `Exp%(Mid)`,
-            `N(Att)` = sum(section == "Att"),
-            `%(Att)` = sum(success[section == "Att"])/`N(Att)`,
-            `Exp%(Att)` = mean(success.pred[section == "Att"]),
-            `Score(Att)` = `%(Att)` - `Exp%(Att)`)
+            success = sum(success),
+            exp = sum(success.pred))
 
-saveRDS(team.stats.offense, "xPassingByTeamOffense.rds")
-saveRDS(team.stats.defense, "xPassingByTeamDefense.rds")
+saveRDS(team.stats.offense, "IgnoreList/xPassingByTeamOffense.rds")
+saveRDS(team.stats.defense, "IgnoreList/xPassingByTeamDefense.rds")
