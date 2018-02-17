cd7299ea6adda1da0786d4e9886dbc35b117db0e
MKullowatz <mkullowatz@gmail.com>
2018-02-17 00:28:17 -0800

    Rewrite to output all separately

--- PasserxPassesFunction.R
+++ PasserxPassesFunction.R
@@ -12,36 +12,38 @@ passer.xpasses <- function(playerpassing,
                            seasonfilter,
                            byteams,
                            byseasons,
-                           third.filter = c("Att", "Def", "Mid")){
+                           third.filter = c("All", "Att", "Def", "Mid")){
  
+  
   playerpassing.temp <- playerpassing %>%
     ungroup() %>%
     filter(year %in% seasonfilter) %>%
-    group_by_(.dots = c("Player" = "passer", "Season" = "year", "team")[c(T, byseasons, byteams)]) %>%
+    group_by_(.dots = c("Player" = "passer", "Season" = "year", "team", "third")[c(T, byseasons, byteams, third.filter != "All")]) %>%
     summarize(Team = paste(unique(team), collapse = ", "),
-              Totals = "Totals",
               Passes = sum(N),
-              PassPct = sum(success)/Passes,
-              xPass = sum(exp)/Passes,
-              Score = (PassPct - xPass)*Passes,
+              PassPct = sum(successes)/Passes,
+              xPassPct = sum(exp)/Passes,
+              Score = (PassPct - xPassPct)*Passes,
               Per100 = Score*100/Passes,
-              Third = third.filter,
-              Passes_ = sum(N[third == third.filter]),
-              PassPct_ = sum(success[third == third.filter])/Passes_,
-              xPass_ = sum(exp[third == third.filter])/Passes_,
-              Score_ = (PassPct_ - xPass_)*Passes_,
-              Per100_ = Score_*100/Passes_) %>%
+              Distance = sum(Distance)/sum(successes),
+              Vertical = sum(Vert.Dist)/sum(successes)) %>%
+    ungroup() %>%
     select(-one_of("team")) %>%
-    filter(Passes_ > minpasses)
-  names(playerpassing.temp) <- gsub("_", " ", names(playerpassing.temp))
+    filter(Passes > minpasses)
+  
+  if(third.filter != "All"){
+  playerpassing.temp <- playerpassing.temp %>%
+    filter(third %in% third.filter) %>%
+    select(-third)
+  }
   return(playerpassing.temp)
   
 }
 
 # Function example:
-passer.xpasses(playerpassing = readRDS("IgnoreList/xPassingByPlayer.rds"),
-               minpasses = 50,
-               seasonfilter = 2015:2016,
-               byteams = T,
-               byseasons = T,
-               third.filter = "Att") %>% as.data.frame() %>% head()
+# passer.xpasses(playerpassing = readRDS("IgnoreList/xPassingByPlayer.rds"),
+#                minpasses = 50,
+#                seasonfilter = 2015:2016,
+#                byteams = T,
+#                byseasons = T,
+#                third.filter = "Att") %>% as.data.frame() %>% head()
