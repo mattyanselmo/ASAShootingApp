c4fd52c25dd7df44ad68fbc5bf2aa4f33f0b6baf
MKullowatz <mkullowatz@gmail.com>
2018-02-22 07:18:53 -0800

    Edit player names

--- 02_AddMinutesToShotCharts.R
+++ 02_AddMinutesToShotCharts.R
@@ -1,7 +1,14 @@
-
+# Produces minutes played per player per game
+# Append dates to minutes data
+library(dplyr)
+library(stringr)
 minutesPlayed <- bind_rows(lapply(grep('minutes played by game', list.files('IgnoreList/'), value = T),
                                   function(x) read.csv(paste0('IgnoreList/', x), stringsAsFactors = F))) %>%
   select(-X) %>%
+  mutate(player = str_replace_all(player, 
+                           c('Kazaishvili' = 'Qazaishvili', 
+                             'Jorge Villafaña' = 'Jorge Villafana',
+                             "Antonio Mlinar Dalamea" = "Antonio Mlinar Delamea"))) %>%
   left_join(playerxgoals %>% select(gameID, date) %>% unique(), by = 'gameID') %>%
   mutate(Season = as.numeric(format(date, '%Y')))
 
