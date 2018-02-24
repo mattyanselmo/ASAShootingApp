a5ee320c0e93cb5f82a0bc9fe85c219e18bb46c1
MKullowatz <mkullowatz@gmail.com>
2018-02-23 16:11:29 -0800

    Call new minutes/teams rds file

--- 00_MasterUpdate.R
+++ 00_MasterUpdate.R
@@ -6,16 +6,14 @@ source('01_ASAShotCharts.R')
 source("01_VerticalLineups.R")
 source("01_ASAPassCharts.R")
 
+source("02_MergeMinutesWithTeam.R")
 source('02_CompressPlayerxGoals.R')
-source('02_AddMinutesToShotCharts.R')
 source('02_CompressTeamxGoals.R')
-
 source('02_CompressPlayerPassing.R')
-source('02_AddMinutesToPassCharts.R')
 source('02_CompressTeamPassing.R')
-
 # Construct predictive model dataset
 # source('03_TeamPMData.R')
 # source('03_TeamPredictiveModelFunction.R')
 source('02_CompressKeeperxGoals.R')
+
 source('IgnoreList/ConnectOnline.R')
