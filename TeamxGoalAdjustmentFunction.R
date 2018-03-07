ac847977bc245fe121f7f41a17585a5d44f7754c
MKullowatz <mkullowatz@gmail.com>
2018-03-06 22:09:43 -0800

    Made more robust by changing time temporarily to numeric

--- TeamxGoalAdjustmentFunction.R
+++ TeamxGoalAdjustmentFunction.R
@@ -1,9 +1,21 @@
 team.xgoal.adj <- function(df, posstime){
-  df %>% 
-    group_by(team, date) %>%
-    arrange(time) %>%
-    mutate(Poss.Num = sapply(1:n(), function(x) x - sum(which(diff(time) < posstime) < x))) %>%
-    group_by(team, date, Poss.Num) %>%
-    mutate(xGTeam = xGTeam*(1 - prod(1 - xGTeam))/sum(xGTeam)) %>%
-    ungroup()
+  if(is.numeric(df$time)){
+    df %>% 
+      group_by(team, date) %>%
+      arrange(time) %>%
+      mutate(Poss.Num = sapply(1:n(), function(x) x - sum(which(diff(time) < posstime) < x))) %>%
+      group_by(team, date, Poss.Num) %>%
+      mutate(xGTeam = xGTeam*(1 - prod(1 - xGTeam))/sum(xGTeam)) %>%
+      ungroup() 
+  } else{
+    df %>% 
+      group_by(team, date) %>%
+      mutate(time2 = sapply(strsplit(time, ':'), function(x) as.numeric(x[1]) + as.numeric(x[2])/60)) %>%
+      arrange(time2) %>%
+      mutate(Poss.Num = sapply(1:n(), function(x) x - sum(which(diff(time2) < posstime) < x))) %>%
+      group_by(team, date, Poss.Num) %>%
+      mutate(xGTeam = xGTeam*(1 - prod(1 - xGTeam))/sum(xGTeam)) %>%
+      select(-time2) %>%
+      ungroup()
+  }
 }
