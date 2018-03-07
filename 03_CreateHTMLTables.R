7e103b3e537720702f52f57110c51a07ac0e1955
MKullowatz <mkullowatz@gmail.com>
2018-03-07 08:19:32 -0800

    Output percentages for player passing Change order of columns for player passing Include some necessary HTML tags
    Note that the percentages get converted back
    to decimals for the HTML file, but this code
    should be used in the app!

--- 03_CreateHTMLTables.R
+++ 03_CreateHTMLTables.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(xtable)
+library(formattable)
 
 playerxgoals <- readRDS('IgnoreList/xGoalsByPlayer.rds')
 minutesPlayed <- readRDS('IgnoreList/MinutesByGameID.rds')
@@ -180,115 +181,120 @@ dt_xG_team <- lapply(2011:max(teamxgoals$Season),
 
 lapply(2015:max(playerpassing$year),
        FUN = function(x){
-        all <- passer.xpasses(playerpassing,
-                        minpasses = 0,
-                        seasonfilter = x,
-                        byteams = F,
-                        byseasons = T,
-                        third.filter = "All") %>%
-          mutate(Per100 = Per100/100) %>%
-          rename(`%` = PassPct, `Exp%` = xPassPct, `%Diff` = Per100)
-        
-        att <- passer.xpasses(playerpassing,
-                              minpasses = 0,
-                              seasonfilter = x,
-                              byteams = F,
-                              byseasons = T,
-                              third.filter = "Att") %>%
-          mutate(Per100 = Per100/100) %>%
-          rename(`%` = PassPct, `Exp%` = xPassPct, `%Diff` = Per100)
-        
-        mid <- passer.xpasses(playerpassing,
-                              minpasses = 0,
-                              seasonfilter = x,
-                              byteams = F,
-                              byseasons = T,
-                              third.filter = "Mid") %>%
-          mutate(Per100 = Per100/100) %>%
-          rename(`%` = PassPct, `Exp%` = xPassPct, `%Diff` = Per100)
-        
-        def <- passer.xpasses(playerpassing,
-                              minpasses = 0,
-                              seasonfilter = x,
-                              byteams = F,
-                              byseasons = T,
-                              third.filter = "Def") %>%
-          mutate(Per100 = Per100/100) %>%
-          rename(`%` = PassPct, `Exp%` = xPassPct, `%Diff` = Per100)
-        
-        dt <- all %>%
-          select(-c(Season, Score, Distance)) %>%
-          full_join(def %>%
-                      select(-c(Season, Team, Pos, Score, Distance)), 
-                    by = c("Player"), 
-                    suffix = c("_all", "_def")) %>%
-          full_join(mid %>%
-                      select(-c(Season, Team, Pos, Score, Distance)), 
-                    by = c("Player")) %>%
-          full_join(att %>%
-                      select(-c(Season, Team, Pos, Score, Distance)), 
-                    by = c("Player"), 
-                    suffix = c("_mid", "_att"))
-        dt <- dt %>%
-          mutate_at(.vars = setdiff(names(dt), c("Player", "Team", "Pos")),
-                    .funs = function(x) ifelse(is.na(x), 0, x))
-        
-        
-        namesFL <- as.data.frame(do.call("rbind", strsplit(sub(" ", ";", dt$Player), ";")))
-        names(namesFL) <- c("First", "Last")
-        
-        dt <- data.frame(namesFL, dt %>% select(-Player), check.names = F)
-        
-        output <- print.xtable(xtable(dt, 
-                         digits = c(0, 0, 0, 0, 0, 0, 3, 3, 3, 2, 0, 3, 3, 3, 2, 0, 3, 3, 3, 2, 0, 3, 3, 3, 2),
-                         align = rep("c", ncol(dt) + 1)),
-                         type = "html",
-                         include.rownames = F,
-                         print.results = F)
-        # replace column tags and include script
-        write.table(gsub("_all|_def|_mid|_att", "",
-                         gsub("<table border=1>", '<script>
+         options(scipen = 100)
+         all <- passer.xpasses(playerpassing,
+                               minpasses = 0,
+                               seasonfilter = x,
+                               byteams = F,
+                               byseasons = T,
+                               third.filter = "All") %>%
+           mutate(Per100 = Per100/100) %>%
+           rename(`%` = PassPct, `Exp%` = xPassPct, `%Diff` = Per100)
+         
+         att <- passer.xpasses(playerpassing,
+                               minpasses = 0,
+                               seasonfilter = x,
+                               byteams = F,
+                               byseasons = T,
+                               third.filter = "Att") %>%
+           mutate(Per100 = Per100/100) %>%
+           rename(`%` = PassPct, `Exp%` = xPassPct, `%Diff` = Per100)
+         
+         mid <- passer.xpasses(playerpassing,
+                               minpasses = 0,
+                               seasonfilter = x,
+                               byteams = F,
+                               byseasons = T,
+                               third.filter = "Mid") %>%
+           mutate(Per100 = Per100/100) %>%
+           rename(`%` = PassPct, `Exp%` = xPassPct, `%Diff` = Per100)
+         
+         def <- passer.xpasses(playerpassing,
+                               minpasses = 0,
+                               seasonfilter = x,
+                               byteams = F,
+                               byseasons = T,
+                               third.filter = "Def") %>%
+           mutate(Per100 = Per100/100) %>%
+           rename(`%` = PassPct, `Exp%` = xPassPct, `%Diff` = Per100)
+         
+         dt <- all %>%
+           select(-c(Season, Score, Distance)) %>%
+           full_join(att %>%
+                       select(-c(Season, Team, Pos, Score, Distance)), 
+                     by = c("Player"), 
+                     suffix = c("_all", "_att")) %>%
+           full_join(mid %>%
+                       select(-c(Season, Team, Pos, Score, Distance)), 
+                     by = c("Player")) %>%
+           full_join(def %>%
+                       select(-c(Season, Team, Pos, Score, Distance)), 
+                     by = c("Player"), 
+                     suffix = c("_mid", "_def"))
+         dt <- dt %>%
+           mutate_at(.vars = setdiff(names(dt), c("Player", "Team", "Pos")),
+                     .funs = function(x) ifelse(is.na(x), 0, x)) %>%
+           mutate_at(.vars = grep("%", names(dt), value = T),
+                     .funs = function(x) percent(x, digits = 1))
+         
+         
+         namesFL <- as.data.frame(do.call("rbind", strsplit(sub(" ", ";", dt$Player), ";")))
+         names(namesFL) <- c("First", "Last")
+         
+         dt <- data.frame(namesFL, dt %>% select(-Player), check.names = F)
+         
+         output <- print.xtable(xtable(dt, 
+                                       digits = c(0, 0, 0, 0, 0, 0, 3, 3, 3, 2, 0, 3, 3, 3, 2, 0, 3, 3, 3, 2, 0, 3, 3, 3, 2),
+                                       align = rep("c", ncol(dt) + 1)),
+                                type = "html",
+                                include.rownames = F,
+                                print.results = F)
+         # replace column tags and include script
+         write.table(gsub("</table>", "</tbody> </table>",
+                          gsub("Vertical </th>  </tr>", "Vertical </th> </tr> </thead> <tbody>",
+                               gsub("_all|_def|_mid|_att", "",
+                                    gsub("<table border=1>", '<script>
 $(document).ready(function() {
 $("#myTable").tablesorter();
   });
 </script>
 <TABLE border=1 id="myTable" class="tablesorter"><thead>
-<TR><TH colspan="4" class="sorter-false"></TH><TH colspan="5" class="sorter-false">All Passes</TH><TH colspan="5" class="sorter-false">Defensive Third</TH><TH colspan="5" class="sorter-false">Middle Third</TH><TH colspan="5" class="sorter-false">Attacking Third</TH></TR>
+<TR><TH colspan="4" class="sorter-false"></TH><TH colspan="5" class="sorter-false">All Passes</TH><TH colspan="5" class="sorter-false">Attacking Third</TH><TH colspan="5" class="sorter-false">Middle Third</TH><TH colspan="5" class="sorter-false">Defensive Third</TH></TR>
 ',
-                         output)),
-                    file = paste0("C:/Users/Matthias.Kullowatz/Dropbox/ASA Blog Data/HTMLOutputs/Player_xPasses_", x, ".txt"),
-                    row.names = F,
-                    quote = F)  
+                                         output)))),
+                               file = paste0("C:/Users/Matthias.Kullowatz/Dropbox/ASA Blog Data/HTMLOutputs/Player_xPasses_", x, ".txt"),
+                               row.names = F,
+                               quote = F)
        })
-  
+
 # Keeper xGoals ####
 # First	Last	Team	Mins	Saves	GA	SOG	xGA	GA-xGA	GA-xGAp96
 lapply(2011:max(keeperxgoals$Season),
        FUN = function(x){
-       dt <- keeperxgoals.func(keeperxgoals %>%
-                             mutate(date = as.Date(date, format = '%m/%d/%Y')),
-                           date1 = as.Date('2000-01-01'),
-                           date2 = as.Date('9999-12-31'),
-                           season = x,
-                           shotfilter = 0,
-                           byteams = F,
-                           byseasons = T,
-                           OtherShots = T,
-                           FK = T,
-                           PK = T) %>%
-         select(-Season) %>%
-         rename(SOG = Shots, GA = Goals, xGA = xG, `GA-xGA` = `G-xG`)
-       
-       output <- xtable(dt, 
-                        digits = c(0, 0, 0, 0, 0, 0, 2, 1, 2, 2),
-                        align = rep("c", ncol(dt) + 1))
-       write.table(gsub("table border=1", 'table border=1 class = "sortable"',
-                        print.xtable(output, 
-                                     type = "html",
-                                     include.rownames = F,
-                                     print.results = F)),
-                   file = paste0("C:/Users/Matthias.Kullowatz/Dropbox/ASA Blog Data/HTMLOutputs/Keeper_xGoals_", x, ".txt"),
-                   row.names = F,
-                   quote = F)
-       
+         dt <- keeperxgoals.func(keeperxgoals %>%
+                                   mutate(date = as.Date(date, format = '%m/%d/%Y')),
+                                 date1 = as.Date('2000-01-01'),
+                                 date2 = as.Date('9999-12-31'),
+                                 season = x,
+                                 shotfilter = 0,
+                                 byteams = F,
+                                 byseasons = T,
+                                 OtherShots = T,
+                                 FK = T,
+                                 PK = T) %>%
+           select(-Season) %>%
+           rename(SOG = Shots, GA = Goals, xGA = xG, `GA-xGA` = `G-xG`)
+         
+         output <- xtable(dt, 
+                          digits = c(0, 0, 0, 0, 0, 0, 2, 1, 2, 2),
+                          align = rep("c", ncol(dt) + 1))
+         write.table(gsub("table border=1", 'table border=1 class = "sortable"',
+                          print.xtable(output, 
+                                       type = "html",
+                                       include.rownames = F,
+                                       print.results = F)),
+                     file = paste0("C:/Users/Matthias.Kullowatz/Dropbox/ASA Blog Data/HTMLOutputs/Keeper_xGoals_", x, ".txt"),
+                     row.names = F,
+                     quote = F)
+         
        })
