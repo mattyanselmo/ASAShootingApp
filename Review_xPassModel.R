0c2c6644b1c306c5aa77fa13b97e3eb5fa47a143
MKullowatz <mkullowatz@gmail.com>
2018-03-19 07:57:13 -0700

    Review the xPass model across all dimensions

---
+++ Review_xPassModel.R
@@ -0,0 +1,36 @@
+# Review xPass model
+
+library(dplyr)
+library(gbm)
+library(stringr)
+library(ggplot2)
+library(scales)
+
+passes <- readRDS("IgnoreList/AllPassingData.rds")
+smod <- readRDS("IgnoreList/xPassModel.rds")
+# Partial plots ####
+plot(smod, i.var = "angle", return.grid = T, type = "response", continuous.resolution = 25) %>%
+  mutate(angle = 180*angle/pi) %>%
+  ggplot(aes(x = angle, y = y)) +
+  geom_line(size = 2) +
+  xlab("Angle (degrees)") + ylab("Completion probability") + ggtitle("Predicted pass success by angle") +
+  scale_y_continuous(labels = scales::percent, limits = c(0, 1)) + 
+  labs(caption = "0 degrees implies forward; positive angles in the counterclockwise direction.") +
+  theme(plot.caption = element_text(hjust = 0.5))
+
+xy.tab <- plot(smod, i.var = c("x", "y"), return.grid = T, type = "response", continuous.resolution = 12)
+names(xy.tab) <- c("x", "y", "pred")
+xy.tab %>%
+  ggplot(aes(x = x, y = y)) + 
+  geom_tile(aes(fill = pred)) + 
+  scale_fill_gradient2(low = "red", mid = "yellow", high = "forestgreen", midpoint = .7) +
+  xlab("X coordinate (yards from own goal)") + ylab("Y coordinate (yards from right sideline)") +
+  ggtitle("Predicted pass success by field position")
+
+# Playerdiff
+# Make table of indicators with observed/predicted rates
+
+# Validation ####
+
+# By field zone
+# By general direction/field zone
