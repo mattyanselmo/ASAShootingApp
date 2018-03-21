b2c140beca607688a380c3301cf08f140df74670
MKullowatz <mkullowatz@gmail.com>
2018-03-21 08:01:06 -0700

    Include xG output from both models

--- xGoalByGameFunction.R
+++ xGoalByGameFunction.R
@@ -1,9 +1,13 @@
 xG.by.game <- function(shooting){
   shooting %>% group_by(date, hteam) %>%
     summarize(ateam = ateam[1], 
-              HxG = sum(xGTeam[hteam == team]),
-              AxG = sum(xGTeam[ateam == team]),
-              xGD = HxG - AxG,
-              GD = final[1])
-  
+              HG = sum((result == "Goal")[hteam == team]),
+              HxGt = sum(xGTeam[hteam == team]),
+              HxGp = sum(xGShooter[hteam == team]),
+              AG = sum((result == "Goal")[ateam == team]),
+              AxGt = sum(xGTeam[ateam == team]),
+              AxGp = sum(xGShooter[ateam == team]),
+              GD = final[1],
+              xGDt = HxGt - AxGt,
+              xGDp = HxGp - AxGp)
 }
