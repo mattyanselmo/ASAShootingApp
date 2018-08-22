c848b345e155495b371e75ea1ba0f39220f3a97c
MKullowatz <matthias.kullowatz@milliman.com>
2018-08-22 08:19:38 -0700

    Compress granular chains data

--- 02_CompressPlayerxGChains.R
+++ 02_CompressPlayerxGChains.R
@@ -5,9 +5,35 @@ library(dplyr)
 library(stringr)
 
 # load xGchain combined data, minutes by game id
+combined <- readRDS("IgnoreList/xGChain_combineddata.rds")
+minutesPlayed_gameID <- readRDS("IgnoreList/MinutesByGameID.rds")
+playerpos <- readRDS("IgnoreList/playerpositions_byseason.rds")
 
 # summarize relevant chain data by player and date
+playerchaindata <- combined %>%
+  group_by(team, gameID) %>%
+  mutate(num.team.chains = length(na.omit(unique(ChainID[outcome == 1 & action %in% c("pass", "dribble", "shot")])))) %>%
+  group_by(player, team, gameID) %>%
+  summarize(date = date[1],
+            num.team.chains = num.team.chains[1],
+            num.chains = length(na.omit(unique(ChainID[outcome == 1 & action %in% c("pass", "dribble", "shot")]))),
+            shots.chain = length(na.omit(unique(ChainID[xG > 0]))),
+            shots = length(na.omit(unique(ChainID[action == "shot"]))),
+            keypasses = length(na.omit(unique(ChainID[keyPass == 1]))),
+            xG.shooter = sum(xGShooter[action == "shot"], na.rm = T),
+            xG.assister = sum(xGShooter[action == "pass" & assist == 1], na.rm = T),
+            xG.buildup.noshots = sum(tapply(xG[action %in% c("pass", "dribble") & outcome == 1 & keyPass != 1], ChainID[action %in% c("pass", "dribble") & outcome == 1 & keyPass != 1], function(x) max(c(0, x)))),
+            xG.buildup.shots = sum(tapply(xG[action %in% c("pass", "dribble", "shot") & outcome == 1], ChainID[action %in% c("pass", "dribble", "shot") & outcome == 1], function(x) max(c(0, x))))) %>%
+  ungroup()
+  
 
 # join on minutes and positions
+playerchaindata <- playerchaindata %>%
+  mutate(year = as.numeric(format(date, "%Y"))) %>%
+  left_join(playerpos, by = c("player" = "passer", "year", "team")) %>%
+  left_join(minutesPlayed_gameID, by = c("player", "team", "gameID")) %>%
+  rename(Season = year)
 
 # write file
+saveRDS(playerchaindata, "IgnoreList/PlayerxGChainData.rds")
+write.csv(playerchaindata, "IgnoreList/PlayerxGChainData.csv", row.names = F)
