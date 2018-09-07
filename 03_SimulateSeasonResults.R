50ce377c471b376b85a160f8505fce109008049b
MKullowatz <matthias.kullowatz@milliman.com>
2018-09-07 15:49:24 -0700

    Simulate playoff seeding probabilities

---
+++ 03_SimulateSeasonResults.R
@@ -0,0 +1,63 @@
+library(dplyr)
+# year <- 2018
+sched <- readRDS("IgnoreList/RemainingSchedule.rds")
+standings <- readRDS("IgnoreList/CurrentStandings.rds")
+pred.data <- readRDS("IgnoreList/TeamPredictionsData.rds")
+load('IgnoreList/UnivariatePoissonModels.Rdata')
+# source("03_TeamPredictiveModelFunction.R")
+
+# About 60 seconds per 100 runs
+N <- 1000
+standings.cum <- data.frame()
+for(run in 1:N){
+  scores <- matrix(NA, nrow = nrow(sched), ncol = 2)
+  standings.temp <- standings
+  for(i in 1:nrow(sched)){
+    score.mat <- pm.function(pred.data = pred.data,
+                             model.home = model.home,
+                             model.away = model.away,
+              team.home = sched$Home[i],
+              team.away = sched$Away[i],
+              season = year)$score.mat
+  
+  cell <- which(cumsum(score.mat) > runif(1))[1]
+  scores[i,] <- c(cell %% 11 - 1, (cell - 1) %/% 11)
+  
+  standings.temp$Pts[standings.temp$Team == sched$Home[i]] <- standings.temp$Pts[standings.temp$Team == sched$Home[i]] + 3*(scores[i,1] > scores[i,2]) + (scores[i,1] == scores[i,2])
+  standings.temp$Pts[standings.temp$Team == sched$Away[i]] <- standings.temp$Pts[standings.temp$Team == sched$Away[i]] + 3*(scores[i,1] < scores[i,2]) + (scores[i,1] == scores[i,2])
+  
+  standings.temp$GF[standings.temp$Team == sched$Home[i]] <- standings.temp$GF[standings.temp$Team == sched$Home[i]] + scores[i,1]
+  standings.temp$GF[standings.temp$Team == sched$Away[i]] <- standings.temp$GF[standings.temp$Team == sched$Away[i]] + scores[i,2]
+  
+  standings.temp$GA[standings.temp$Team == sched$Home[i]] <- standings.temp$GA[standings.temp$Team == sched$Home[i]] + scores[i,2]
+  standings.temp$GA[standings.temp$Team == sched$Away[i]] <- standings.temp$GA[standings.temp$Team == sched$Away[i]] + scores[i,1]
+  
+  standings.temp$Wins[standings.temp$Team == sched$Home[i]] <- standings.temp$Wins[standings.temp$Team == sched$Home[i]] + (scores[i,1] > scores[i,2])
+  standings.temp$Wins[standings.temp$Team == sched$Away[i]] <- standings.temp$Wins[standings.temp$Team == sched$Away[i]] + (scores[i,2] > scores[i,1])
+  
+  standings.temp$Games[standings.temp$Team == sched$Home[i]] <- standings.temp$Games[standings.temp$Team == sched$Home[i]] + 1
+  standings.temp$Games[standings.temp$Team == sched$Away[i]] <- standings.temp$Games[standings.temp$Team == sched$Away[i]] + 1
+  
+}
+standings.cum <- bind_rows(standings.cum, standings.temp %>% mutate(Run = run))
+gc()
+}
+
+results <- standings.cum %>%
+  group_by(Run, Conf) %>%
+  arrange(desc(Pts), desc(Wins), desc(GF - GA)) %>%
+  mutate(Rank = pmin(7, 1:n()))
+
+final.pos <- results %>% 
+  group_by(Team, Conf) %>%
+  summarize(`1` = mean(Rank == 1),
+            `2` = mean(Rank == 2),
+            `3` = mean(Rank == 3),
+            `4` = mean(Rank == 4),
+            `5` = mean(Rank == 5),
+            `6` = mean(Rank == 6),
+            `7+` = mean(Rank >= 7)) %>%
+  arrange(desc(`1`), desc(`2`), desc(`3`), desc(`4`), desc(`5`), desc(`6`))
+
+final.pos
+saveRDS(final.pos, "IgnoreList/CurrentSimulationResults_playoffseeding.rds")
