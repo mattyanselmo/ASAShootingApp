b3669f65328511981bc5ff3ef6a9d83ea1896c08
MKullowatz <matthias.kullowatz@milliman.com>
2018-09-07 15:53:46 -0700

    Prepare game prediction model (bivariate poisson)

--- 03_TeamPredictiveModelFunction.R
+++ 03_TeamPredictiveModelFunction.R
@@ -8,9 +8,9 @@
 pm.function <- function(pred.data, model.home, model.away, team.home, team.away, season = 2017){
   
   temp.home <- pred.data %>%
-    filter(team == team.home, Season == season) %>%
-    mutate(xGF_season_team = )
+    filter(team == team.home, Season == season)
   names(temp.home) <- paste0(names(temp.home), '_home')
+  
   temp.away <- pred.data %>%
     filter(team == team.away, Season == season)
   names(temp.away) <- paste0(names(temp.away), '_away')
@@ -29,20 +29,20 @@ pm.function <- function(pred.data, model.home, model.away, team.home, team.away,
   dist.away <- sapply(0:10, function(x) dpois(x, lambda = lambda.away))
   
   rho <- -0.06
-    temp <-  dist.home %o% dist.away
-    temp[1, 1] <- temp[1, 1] * (1 - lambda.home*lambda.away*rho)
-    temp[1, 2] <- temp[1, 2] * (1 + lambda.home*rho)
-    temp[2, 1] <- temp[2, 1] * (1 + lambda.away*rho)
-    temp[2, 2] <- temp[2, 2] * (1 - rho)
-    score.mat <- temp / sum(temp)
+  temp <-  dist.home %o% dist.away
+  temp[1, 1] <- temp[1, 1] * (1 - lambda.home*lambda.away*rho)
+  temp[1, 2] <- temp[1, 2] * (1 + lambda.home*rho)
+  temp[2, 1] <- temp[2, 1] * (1 + lambda.away*rho)
+  temp[2, 2] <- temp[2, 2] * (1 - rho)
+  score.mat <- temp / sum(temp)
   
   #####
   prob.draw <- sum(diag(score.mat))
   prob.away <- sum(score.mat[upper.tri(score.mat)])
   prob.home <- sum(score.mat[lower.tri(score.mat)])
   
-  return(list(score.mat, probs = c(prob.home, prob.away, prob.draw)))
-
+  return(list(score.mat = score.mat, probs = c(prob.home, prob.away, prob.draw)))
+  
 }
 
-pm.function(pred.data, model.home, model.away, 'SEA', 'VAN')
+# pm.function(pred.data, model.home, model.away, 'SEA', 'VAN')
