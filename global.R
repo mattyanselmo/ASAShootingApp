d081dd5e0744c01d063d856f105d4ea4da7650f7
Matthias Kullowatz <Matthias.Kullowatz@milliman.com>
2019-03-22 19:19:44 -0700

    Include home-adjusted team xgoals

--- global.R
+++ global.R
@@ -40,6 +40,7 @@ playerxgoals <- readRDS('AppData/xGoalsByPlayer.rds')
 minutesPlayed <- readRDS('AppData/MinutesByGameID_forapp.rds') %>%
   filter(Season >= seasonFilter)
 teamxgoals <- readRDS('AppData/xGoalsByTeam.rds')
+teamxgoals.adj <- readRDS("AppData/xGoalsByTeam_HomeAdjusted.rds")
 xgbygame <- readRDS('AppData/xGoalsByTeam_byGame.rds')
 keeperxgoals <- readRDS('AppData/xGoalsByKeeper.rds')
 conferences <- read.csv('AppData/teamsbyconferencebyseason.csv')
