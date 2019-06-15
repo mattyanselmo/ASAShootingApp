fda51750d6720fdfe4ee2215e14c26a26d635117
Matthias Kullowatz <Matthias.Kullowatz@milliman.com>
2019-06-15 14:43:18 -0700

    Include @analysisevolved handle

--- Functions/PlotWinProbCharts.R
+++ Functions/PlotWinProbCharts.R
@@ -10,7 +10,8 @@
 # app <- "master"
 # action.data <- readRDS(paste0("ASAShootingApp_", app, "/AppData/WinProbabilityActions_bygame.rds"))
 # action.input <- action.data %>%
-#   filter(gameID == 914862)
+#   filter(gameID == 914862) %>%
+#   filter(Action == "lastminute")
 # winmodel <- winmodel.purged
 # drawmodel <- drawmodel.purged
 
@@ -18,13 +19,20 @@ winprobchart.func <- function(action.input,
                               winmodel,
                               drawmodel){
   
+  annotations <- data.frame(
+    xpos = c(0),
+    ypos =  c(0.95),
+    annotateText = c("@AnalysisEvolved"))
+  
+  
   last.minute <- action.input$minute[action.input$Action == "lastminute"]
   hteam.fixed <- tail(action.input$hteam, 1)
+  hteam.fixed <- ifelse(hteam.fixed == "", " ", hteam.fixed)
   ateam.fixed <- tail(action.input$ateam, 1)
   final.fixed <- tail(action.input$final, 1)
   date.fixed <- tail(action.input$date, 1)
   
-  action.data <- action.input %>%
+  action.input <- action.input %>%
     filter(Action != "lastminute")
   
   plot.data <- data.frame(minute = 0,
@@ -53,16 +61,15 @@ winprobchart.func <- function(action.input,
                                     ifelse(variable == "Loss",
                                            paste0(ateam.fixed, " win"),
                                            "Draw")), 
-                             levels = c(paste0(hteam.fixed, " win"),
-                                        "Draw",
-                                        paste0(ateam.fixed, " win"))))
+                             levels = unique(c(paste0(hteam.fixed, " win"),
+                                               "Draw",
+                                               paste0(ateam.fixed, " win")))))
   
-  p <- ggplot(plot.data, aes(x = minute, y = value, color = variable, fill = variable))+
+  p <- ggplot(plot.data, aes(x = minute, y = value, color =  variable, fill = variable))+
     geom_area(alpha = 0.5)+
     labs(y = "Probability", 
          x = "Minute", 
          title = paste0(hteam.fixed, " vs. ", ateam.fixed, "    ", date.fixed)) +
-    
     scale_y_continuous(labels = scales::percent, limits = c(0, 1)) +
     scale_x_continuous(breaks = seq(15, 90, 15)) +
     scale_fill_manual("Outcome",
@@ -83,14 +90,14 @@ winprobchart.func <- function(action.input,
                color = "black", 
                linetype = "dotted")
   
-  if(nrow(action.data) > 0){
+  if(nrow(action.input) > 0){
     p +
-      geom_vline(data=action.data, 
+      geom_vline(data = action.input, 
                  aes(xintercept = minute), 
                  color = "black", 
                  linetype = "dashed",
                  alpha = 0.25) +
-      geom_text(data=action.data, 
+      geom_text(data = action.input, 
                 inherit.aes = FALSE, 
                 aes(x=minute, 
                     y=0.02, 
@@ -112,7 +119,14 @@ winprobchart.func <- function(action.input,
             legend.position = "right",
             legend.text = element_text(face = "plain", size = 12),
             legend.key = element_rect(fill = "white"),
-            legend.key.size = unit(1, "cm"))
+            legend.key.size = unit(1, "cm")) +
+      geom_text(data = annotations, 
+                inherit.aes = FALSE,
+                aes(x=xpos,
+                    y=ypos,
+                    hjust = rep(0, nrow(annotations)),
+                    label=annotateText),
+                alpha = 0.5)
   } else{
     p + theme_minimal() +
       theme(text = element_text(face = "plain", size = 14),
@@ -126,7 +140,15 @@ winprobchart.func <- function(action.input,
             legend.position = "right",
             legend.text = element_text(face = "plain", size = 12),
             legend.key = element_rect(fill = "white"),
-            legend.key.size = unit(1, "cm"))
+            legend.key.size = unit(1, "cm")) +
+      geom_text(data = annotations, 
+                inherit.aes = FALSE,
+                aes(x=xpos,
+                    y=ypos,
+                    hjust = rep(0, nrow(annotations)),
+                    label=annotateText),
+                color = "black",
+                alpha = 0.5)
   }
 }
 
