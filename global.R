db8a0a1ad558aa3904dc6f10dfa066f601b99c34
Matthias Kullowatz <Matthias.Kullowatz@milliman.com>
2019-10-09 08:04:27 -0700

    Load in separate weeks for cup vs. playoff seeding

--- global.R
+++ global.R
@@ -11,6 +11,7 @@ library(zoo)
 library(reshape2)
 library(stringr)
 
+# For per-minute stats, positions, and other "new" info, input the first year we have that:
 seasonFilter <- 2015
 
 playerxgoals <- readRDS('AppData/xGoalsByPlayer.rds') 
@@ -80,9 +81,14 @@ playerchaindata <- readRDS("AppData/PlayerxGChainData.rds") %>%
 
 current.year <- max(playerxgoals$Season)
 simfiles <- grep("CurrentSimulationResults", list.files("AppData"), value = T)
+cupfiles <- grep("MLSCupSimulationResults", list.files("AppData"), value = T)
 simfiles.weeks <- str_extract(simfiles, "(?<=_week)[0-9]*")
 simfiles.years <- str_extract(simfiles, "(?<=_year)[0-9]*")
+cupfiles.weeks <- str_extract(cupfiles, "(?<=_week)[0-9]*")
+cupfiles.years <- str_extract(cupfiles, "(?<=_year)[0-9]*")
+
 current.week <- max(as.numeric(simfiles.weeks)[simfiles.years == as.character(current.year)], na.rm = T)
+current.week.cup <- max(as.numeric(cupfiles.weeks)[cupfiles.years == as.character(current.year)], na.rm = T) 
 
 weeklypreds <- readRDS(paste0("AppData/WeeklyMatchPredictions_week", current.week, "_year", current.year, ".rds"))
 
@@ -101,12 +107,12 @@ playoffsseeding_east_last <- readRDS(paste0("AppData/CurrentSimulationResults_pl
                                             "_year", current.year, ".rds"))
 
 cupchances <- readRDS(paste0("AppData/MLSCupSimulationResults_week", 
-                             current.week,
+                             current.week.cup,
                              "_year",
                              current.year,
                              ".rds"))
 cupchances_last <- readRDS(paste0("AppData/MLSCupSimulationResults_week", 
-                             current.week - 1,
+                             current.week.cup - 1,
                              "_year",
                              current.year,
                              ".rds"))
